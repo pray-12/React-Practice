d7a8a724bf7c49907876221134cb43169118277f
preethyray <preethy@specbee.com>
2025-07-26 17:01:04 +0000

    initial commit

---
+++ src/Greet.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+// function Greet() {
+//     return <h1>Hello World</h1>
+// }
+
+// export default Greet
+
+// ------------------------------------------------------------
+// const Greet = () => {
+//     return <h1>Hello Ray</h1>
+// }
+
+// export default Greet
+
+// ------------------------------------------------------------
+
+const Greet= function() {
+    return <h2>Hello Ray</h2>
+}
+
+export default Greet
---
+++ src/Welcome.jsx
@@ -0,0 +1,19 @@
+// import React, {Component} from 'react'
+
+// class Welcome extends Component{
+//    render(){
+//     return <p> Hi how are yo u</p>
+//    }
+// }
+
+// export default Welcome
+
+import React from 'react'
+
+class Welcome extends React.Component{
+  render(){
+    return <p> Hi how are yo u ray</p>
+  }
+}
+
+export default Welcome
